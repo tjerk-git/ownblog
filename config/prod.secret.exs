8ac236c1d4d232b28bec047951c1c802ad3767a7
Tjerk <tjerk@hey.com>
2020-12-01 22:35:01 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :vantjerk, Vantjerk.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
